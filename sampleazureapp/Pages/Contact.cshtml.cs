8d34abfa95a65c9f14e4820c7619a2535b2ed5e1
yena816 <yena816@gmail.com>
2023-03-15 12:43:44 -0700

    sql connection

--- sampleazureapp/Pages/Contact.cshtml.cs
+++ sampleazureapp/Pages/Contact.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace sampleazureapp.Pages
 {
@@ -19,6 +21,16 @@ namespace sampleazureapp.Pages
             firstName = Request.Form["firstname"];
             lastName = Request.Form["lastname"];
             message = Request.Form["message"];
+
+            string newquery = "INSERT INTO contact (firstname, lastname, msg) VALUES ('" + firstName + "', '" + lastName + "', '" + message + "');";
+
+            using (SqlConnection sqlConnection = new SqlConnection("Server = tcp:sampleserver92734.database.windows.net,1433; Initial Catalog = sampleappdb; Persist Security Info = False; User ID = sampleadmin; Password = P@ssw0rd123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"))
+            {
+                SqlCommand cmd = new SqlCommand(newquery, sqlConnection);
+                sqlConnection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+            }
         }
     }
 }
