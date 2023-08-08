80df30e51d7b474f41c52ab9a01a6f70e325308b
yena816 <yena816@gmail.com>
2023-08-08 16:12:32 -0700

    Added key vault secret (password to database)

--- sampleazureapp/Pages/Contact.cshtml.cs
+++ sampleazureapp/Pages/Contact.cshtml.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Configuration;
 using System.Data.SqlClient;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 
 namespace sampleazureapp.Pages
 {
@@ -22,9 +25,27 @@ namespace sampleazureapp.Pages
             lastName = Request.Form["lastname"];
             message = Request.Form["message"];
 
+            //get secret
+            SecretClientOptions options = new SecretClientOptions()
+                {
+                    Retry =
+                    {
+                        Delay= TimeSpan.FromSeconds(2),
+                        MaxDelay = TimeSpan.FromSeconds(16),
+                        MaxRetries = 5,
+                        Mode = RetryMode.Exponential
+                    }
+                };
+            var client = new SecretClient(new Uri("https://keyvault38c64193.vault.azure.net/"), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret("samplepassword");
+
+            string secretValue = secret.Value;
+
+            //query 
             string newquery = "INSERT INTO contact (firstname, lastname, msg) VALUES ('" + firstName + "', '" + lastName + "', '" + message + "');";
 
-            using (SqlConnection sqlConnection = new SqlConnection("Server = tcp:sampleserver92734.database.windows.net,1433; Initial Catalog = sampleappdb; Persist Security Info = False; User ID = sampleadmin; Password = P@ssw0rd123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"))
+            using (SqlConnection sqlConnection = new SqlConnection("Server = tcp:sampleserver92734.database.windows.net,1433; Initial Catalog = sampleappdb; Persist Security Info = False; User ID = sampleadmin; Password = "+secretValue+"; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;"))
             {
                 SqlCommand cmd = new SqlCommand(newquery, sqlConnection);
                 sqlConnection.Open();
